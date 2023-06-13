d1841c6c45e1d26958e3afb3bf61e71bfbb21ad4
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-06-13 16:20:06 +0200

    ale-stars-better

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,8 +1,32 @@
 let stars = document.getElementsByClassName("star");
 stars = Array.from(stars);
-let selectedStar;
+let selectedStar = 0;
 
-//viene chiamata ogni volta che la stella viene cliccata o il mouse ci passa sopra
+//viene chiamata ogni volta che il mouse  passa sopra la stella
+const starSelect = function () {
+  for (i = 0; i < this.dataset.value; i++)
+    if (i < this.dataset.value) {
+      stars[i].classList.add("starsSelecteds");
+    }
+};
+
+//viene chiamata quando il mouse esce dalle stelle per cancellare il colore
+const starDeSelected = function () {
+  // if (selectedStar === 0) {
+  let x = 0;
+  let y = 9 - selectedStar;
+  for (i = 0; i < y + 1; i++) {
+    x = 9 - i;
+    //rimuove l'eventuale classe da tutti gli elementi
+    if (stars[x].classList.contains("starsSelecteds")) {
+      stars[x].classList.remove("starsSelecteds");
+      console.log("selectedStar: ", selectedStar, "Deselected x:", x, "i: ", i);
+    }
+  }
+  // }
+};
+
+//viene chiamata ogni volta che la stella viene cliccata
 const starClicked = function (a) {
   stars.forEach((s) => {
     //rimuove l'eventuale classe da tutti gli elementi
@@ -17,10 +41,11 @@ const starClicked = function (a) {
     }
   selectedStar = this.dataset.value;
 };
-//aggiunge alle stelle gli eventi click e mouseover
+//aggiunge alle stelle gli eventi click e mouseover e mouseout
 stars.forEach((a) => {
   a.addEventListener("click", starClicked);
-  a.addEventListener("mouseover", starClicked);
+  a.addEventListener("mouseleave", starDeSelected);
+  a.addEventListener("mouseover", starSelect);
 });
 
 //gestione del form
