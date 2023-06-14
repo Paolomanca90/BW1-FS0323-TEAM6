57484edb14c0e1fb5737d0f07e347265a7bc770c
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-06-14 16:46:38 +0200

    ale-result

--- scripts/results.js
+++ scripts/results.js
@@ -8,41 +8,44 @@ const rightQuestions = localStorage.getItem("rightQuestions");
 const allQuestions = localStorage.getItem("allQuestions");
 
 // variabili per il calcolo delle %
-let rightAns = 9;
+let rightAns = 3;
 let questions = allQuestions;
 let errorAns = questions - rightAns;
 let rightPerc = (100 * rightAns) / questions;
 let opposite = 100 - rightPerc;
 let myResult = document.getElementById("result");
-let svg = document.querySelector('svg')
+let svg = document.querySelector("svg");
 
 // funzione per creare i 3 div dei risultati
 const result = function () {
   // div di sx delle risposte corrette
-  let right = document.createElement('div')
-  let rightP = document.createElement('p')
-  let rightS = document.createElement('p')
-  rightP.innerText = `${rightAns}/${questions} questions`
-  rightS.innerText = `${rightPerc}%`
-  right.innerText = `Correct`
-  right.appendChild(rightS)
-  right.appendChild(rightP)
-  right.classList.add('text')
+  let right = document.createElement("div");
+  let rightP = document.createElement("p");
+  let rightS = document.createElement("p");
+  rightP.innerText = `${rightAns}/${questions} questions`;
+  rightS.innerText = `${rightPerc}%`;
+  right.innerText = `Correct`;
+  right.appendChild(rightS);
+  right.appendChild(rightP);
+  right.classList.add("text");
 
   // div di dx delle risposte errate
-  let error = document.createElement('div')
-  let errorP = document.createElement('p')
-  let errorS = document.createElement('p')
-  errorP.innerText = `${errorAns}/${questions} questions`
-  errorS.innerText = `${opposite}%`
-  error.innerText = `Wrong`
-  error.appendChild(errorS)
-  error.appendChild(errorP)
-  error.classList.add('text')
+  let error = document.createElement("div");
+  let errorP = document.createElement("p");
+  let errorS = document.createElement("p");
+  errorP.innerText = `${errorAns}/${questions} questions`;
+  errorS.innerText = `${opposite}%`;
+  error.innerText = `Wrong`;
+  error.appendChild(errorS);
+  error.appendChild(errorP);
+  error.classList.add("text");
 
   // div centrale
-  let circularDiv = document.createElement('div')
-  circularDiv.classList.add('circular')
+  let circularDiv = document.createElement("div");
+  circularDiv.classList.add("circular");
+
+  let track = document.getElementsByClassName("track")[0];
+  let progress = document.getElementsByClassName("progress")[0];
 
   // appendo i 3 div a cascata per averli nelle giuste posizioni
   myResult.appendChild(right);
@@ -50,37 +53,40 @@ const result = function () {
   myResult.appendChild(error);
 
   // creazione dei gradienti circolari
-  if(rightAns >= 6){
-    let div = document.createElement('div')
-    div.classList.add('finalR')
-    let p = document.createElement('p')
-    let s = document.createElement('p')
-    let f = document.createElement('p')
-    p.innerText = `Congratulations!`
-    s.innerText = `You passed the exam.`
-    f.innerText = `We'll send the certificate in few minutes. Check your email (including promotions / spam folder)`
+  if (rightAns >= 6) {
+    let div = document.createElement("div");
+    div.classList.add("finalR");
+    let p = document.createElement("p");
+    let s = document.createElement("p");
+    let f = document.createElement("p");
+    p.innerText = `Congratulations!`;
+    s.innerText = `You passed the exam.`;
+    f.innerText = `We'll send the certificate in few minutes. Check your email (including promotions / spam folder)`;
     // circularDiv.style = `background: linear-gradient(#642669, #642669) content-box no-repeat, conic-gradient( #D20094 ${opposite}%, 0, #00FFFF) border-box`;
-    circularDiv.appendChild(svg)
-    div.appendChild(p)
-    div.appendChild(s)
-    div.appendChild(f)
-    circularDiv.appendChild(div)
-    
-  }else{
-    let div = document.createElement('div')
-    div.classList.add('finalE')
-    let p = document.createElement('p')
-    let s = document.createElement('p')
-    let f = document.createElement('p')
-    p.innerText = `OPS!`
-    s.innerText = `Unluckly you not passed the exam.`
-    f.innerText = `I'm sure you are not taking the Epicode course with Stefano`
+    circularDiv.appendChild(svg);
+    div.appendChild(p);
+    div.appendChild(s);
+    div.appendChild(f);
+    circularDiv.appendChild(div);
+    progress.style = "stroke: #00ffff;";
+    track.style = "stroke: #d20094";
+  } else {
+    let div = document.createElement("div");
+    div.classList.add("finalE");
+    let p = document.createElement("p");
+    let s = document.createElement("p");
+    let f = document.createElement("p");
+    p.innerText = `OPS!`;
+    s.innerText = `Unluckly you not passed the exam.`;
+    f.innerText = `I'm sure you are not taking the Epicode course with Stefano`;
     // circularDiv.style = `background: linear-gradient(#642669, #642669) content-box no-repeat, conic-gradient( #D20094 ${opposite}%, 0, #00FFFF) border-box`;
-    circularDiv.appendChild(svg)
-    div.appendChild(p)
-    div.appendChild(s)
-    div.appendChild(f)
-    circularDiv.appendChild(div)
+    circularDiv.appendChild(svg);
+    div.appendChild(p);
+    div.appendChild(s);
+    div.appendChild(f);
+    circularDiv.appendChild(div);
+    track.style = "stroke: #00ffff;";
+    progress.style = "stroke: #d20094";
     // circularDiv.setAttribute('data', p.innerText + s.innerText)
   }
   return circularDiv;
@@ -88,13 +94,19 @@ const result = function () {
 
 result();
 
-  let progressC = document.querySelector('.progress')
-  let radius = progressC.r.baseVal.value;
-  let circum = radius * 2 * Math.PI
-  progressC.style.strokeDasharray = circum;
+let progressC = document.querySelector(".progress");
+let radius = progressC.r.baseVal.value;
+let circum = radius * 2 * Math.PI;
+progressC.style.strokeDasharray = circum;
 
-  const setProgress = function(percent){
-    progressC.style.strokeDasharray = circum - (percent/100) * circum;
-  }
+const setProgress = function (percent) {
+  progressC.style.strokeDasharray = circum - (percent / 100) * circum;
+};
 
-  setProgress(rightPerc)
+let setRighPerc = () => {
+  if (rightPerc < 50) {
+    return rightPerc;
+  } else return opposite;
+};
+
+setProgress(setRighPerc());
