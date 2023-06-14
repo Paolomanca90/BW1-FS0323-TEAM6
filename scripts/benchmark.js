a7467c624f6c906741cbd4b8c2c301e5730beb76
AlessioDiPretoro <alessiodipretoro@gmail.com>
2023-06-14 11:19:32 +0200

    ale-benchmark-script

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,99 +1,3 @@
-// const questions = [
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question: "What does CPU stand for?",
-//     correct_answer: "Central Processing Unit",
-//     incorrect_answers: [
-//       "Central Process Unit",
-//       "Computer Personal Unit",
-//       "Central Processor Unit",
-//     ],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question:
-//       "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
-//     correct_answer: "Final",
-//     incorrect_answers: ["Static", "Private", "Public"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "boolean",
-//     difficulty: "easy",
-//     question: "The logo for Snapchat is a Bell.",
-//     correct_answer: "False",
-//     incorrect_answers: ["True"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "boolean",
-//     difficulty: "easy",
-//     question:
-//       "Pointers were not used in the original C programming language; they were added later on in C++.",
-//     correct_answer: "False",
-//     incorrect_answers: ["True"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question:
-//       "What is the most preferred image format used for logos in the Wikimedia database?",
-//     correct_answer: ".svg",
-//     incorrect_answers: [".png", ".jpeg", ".gif"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question: "In web design, what does CSS stand for?",
-//     correct_answer: "Cascading Style Sheet",
-//     incorrect_answers: [
-//       "Counter Strike: Source",
-//       "Corrective Style Sheet",
-//       "Computer Style Sheet",
-//     ],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question:
-//       "What is the code name for the mobile operating system Android 7.0?",
-//     correct_answer: "Nougat",
-//     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question: "On Twitter, what is the character limit for a Tweet?",
-//     correct_answer: "140",
-//     incorrect_answers: ["120", "160", "100"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "boolean",
-//     difficulty: "easy",
-//     question: "Linux was first created as an alternative to Windows XP.",
-//     correct_answer: "False",
-//     incorrect_answers: ["True"],
-//   },
-//   {
-//     category: "Science: Computers",
-//     type: "multiple",
-//     difficulty: "easy",
-//     question:
-//       "Which programming language shares its name with an island in Indonesia?",
-//     correct_answer: "Java",
-//     incorrect_answers: ["Python", "C", "Jakarta"],
-//   },
-// ];
-
 let questions = [];
 let maxSeconds = 30;
 let nowSeconds = maxSeconds;
@@ -157,6 +61,8 @@ const buttonNext = () => {
     } else {
       selectedAns.classList.add("wrongAnswer");
     }
+    //nasconde il bottone next per evitare multiple risposte
+    nextButton.classList.add("none");
     if (questDid.length < questions.length) {
       //e dopo x secondi chiama la prossima domanda o termina il test
       setTimeout(function () {
@@ -221,6 +127,9 @@ const searchAnswers = () => {
 // crea le domande e le risposte in base all'array
 const quest = () => {
   let rnd = Math.floor(Math.random() * questions.length);
+  //controlla se il bottone next non è visibile e lo rende visibile
+  if (nextButton.classList.contains("none"))
+    nextButton.classList.remove("none");
   if (questDid.length < questions.length) {
     if (!questDid.includes(rnd)) {
       nowSeconds = maxSeconds;
